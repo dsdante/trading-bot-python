5e1ac1f03a55b086ba71fb81b71c1ec6d9eb1cf1
Dante <dsdante@protonmail.com>
2023-02-05 21:19:11 +0300

    Clean-up

--- trading_bot.py
+++ trading_bot.py
@@ -2,7 +2,6 @@
 import db
 
 if __name__ == '__main__':
-    db.deploy()
-    db.download_instrument_info()
+    #db.deploy()
+    #db.download_instrument_info()
     db.download_history()
-    # todo: tee broken pipe
